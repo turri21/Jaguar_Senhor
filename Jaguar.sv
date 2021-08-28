82ff153dfa412a81b84871851d527f8966534a34
Jamie Blanks <kitrinx@gmail.com>
2021-08-28 19:16:21 -0400

    fix PLL

--- Jaguar.sv
+++ Jaguar.sv
@@ -202,7 +202,7 @@ assign LED_USER  = ioctl_download;
 `define RDY_WAIT  4'b0001
 `define RAM_END   4'b1111
 
-wire clk_106m;
+wire clk_106m, clk_26m;
 
 wire pll_locked;
 pll pll
@@ -210,18 +210,14 @@ pll pll
 	.refclk(CLK_50M),
 	.rst(0),
 	.outclk_0(clk_106m),
+	.outclk_1(clk_26m),
 	.locked(pll_locked)
 );
 
-
 wire clk_sys = clk_106m;
+wire clk_ram = clk_106m;
 
-
-wire [1:0] scale = status[3:2];
-
-// assign VIDEO_ARX = status[1] ? 8'd16 : 8'd4;
-// assign VIDEO_ARY = status[1] ? 8'd9  : 8'd3;
-
+wire [1:0] scale = status[10:9];
 wire [1:0] ar = status[8:7];
 
 assign VIDEO_ARX = (!ar) ? 12'd4 : (ar - 1'd1);
@@ -234,7 +230,7 @@ assign VIDEO_ARY = (!ar) ? 12'd3 : 12'd0;
 // 0123456789ABCDEFGHIJKLMNOPQRSTUV 0123456789ABCDEFGHIJKLMNOPQRSTUV
 // X XXXXXXX
 
-// 	"O24,Scandoubler Fx,None,HQ2x,CRT 25%,CRT 50%,CRT 75%;",
+// 	
 
 `include "build_id.v"
 localparam CONF_STR = {
@@ -246,6 +242,7 @@ localparam CONF_STR = {
 	"O4,Region Setting,NTSC,PAL;",
 	"O2,Cart Checksum Patch,Off,On;",
 	"O78,Aspect ratio,Original,Full Screen,[ARC1],[ARC2];",
+	"O9A,Scandoubler Fx,None,HQ2x,CRT 25%,CRT 50%,CRT 75%;",
 	"O56,Mouse,Disabled,JoyPort1,JoyPort2;",
 	"O3,CPU Speed,Normal,Turbo;",
 	"-;",
@@ -272,6 +269,7 @@ wire [10:0] ps2_key;
 wire [24:0] ps2_mouse;
 wire [21:0] gamma_bus;
 wire [15:0] sdram_sz;
+
 hps_io #(.CONF_STR(CONF_STR), .PS2DIV(1000), .WIDE(1)) hps_io
 (
 	.clk_sys(clk_sys),
@@ -325,6 +323,8 @@ reg [1:0] status_reg = 0;
 reg       old_download;
 integer   timeout = 0;
 
+wire rom_index = ioctl_index[5:0] == 1;
+wire rom_download = ioctl_download && rom_index;
 
 always @(posedge clk_sys or posedge reset)
 if (reset) begin
@@ -342,9 +342,9 @@ else begin
 
 	loader_wr <= 0;	// Default!
 
-	if (~old_download && ioctl_download && ioctl_index[5:0] == 1) begin
-		loader_addr <= 32'h0080_0000;								// Force the cart ROM to load at 0x00800000 in DDR for Jag core. (byte address!)
-																			// (The ROM actually gets written at 0x30800000 in DDR, which is done when load_addr gets assigned to DDRAM_ADDR below).
+	if (~old_download && ioctl_download && rom_index) begin
+		loader_addr <= 32'h0080_0000;   // Force the cart ROM to load at 0x00800000 in DDR for Jag core. (byte address!)
+		                                // (The ROM actually gets written at 0x30800000 in DDR, which is done when load_addr gets assigned to DDRAM_ADDR below).
 		loader_en <= 1;
 		status_reg <= 0;
 		ioctl_wait <= 0;
@@ -352,9 +352,9 @@ else begin
 		cnt <= 0;
 	end
 
-	if (loader_wr) loader_addr <= loader_addr + 2;				// Writing a 16-bit WORD at a time!
+	if (loader_wr) loader_addr <= loader_addr + 2'd2; // Writing a 16-bit WORD at a time!
 
-	if (ioctl_wr && ioctl_index[5:0] == 1) begin
+	if (ioctl_wr && ioctl_index) begin
 		loader_wr <= 1;
 		ioctl_wait <= 1;
 	end
@@ -374,7 +374,7 @@ else begin
 	end
 */
 
-	if(old_download & ~ioctl_download) begin
+	if(old_download && ~ioctl_download) begin
 		loader_en <= 0;
 		ioctl_wait <= 0;
 	end
@@ -383,7 +383,6 @@ end
 
 wire reset = RESET | status[0] | buttons[1];
 
-//wire xresetl = !(reset | loader_en);		// Forces reset only when ioctl_index>0, for cart load.
 wire xresetl = !(reset | ioctl_download);	// Forces reset on BIOS (boot.rom) load (ioctl_index==0), AND cart ROM.
 wire [9:0] dram_a;
 wire dram_ras_n;
@@ -506,6 +505,20 @@ jaguar jaguar_inst
 	.mouse_ena_2( status[6:5]==2 )
 );
 
+	// output  wire         bridge_m0_waitrequest,
+	// output  wire [31:0]  bridge_m0_readdata,
+	// output  reg          bridge_m0_readdatavalid,
+	// input   wire [6:0]   bridge_m0_burstcount,
+	// input   wire [31:0]  bridge_m0_writedata,
+	// input   wire [19:0]  bridge_m0_address,
+	// input   wire         bridge_m0_write,
+	// input   wire         bridge_m0_read,
+	// input   wire         bridge_m0_byteenable,
+	// output  wire         bridge_m0_clk
+// assign cpu_clken_dbg = 1'b1;
+// assign bridge_m0_waitrequest = 1'b0;
+// assign bridge_m0_readdatavalid = bridge_m0_read;
+
 //wire [1:0] romwidth = status[5:4];
 wire [1:0] romwidth = 2'd2;
 
@@ -517,7 +530,7 @@ assign cpu_clken_dbg = 1'b1;
 //wire os_rom_oe_n;
 //wire os_rom_oe = (~os_rom_ce_n & ~os_rom_oe_n);	// os_rom_oe feeds back TO the core, to enable the internal drivers.
 
-wire os_download = ioctl_download && (ioctl_index[5:0] == 0 || ioctl_index[5:0] == 2);
+wire os_download = ioctl_download && (ioctl_index[5:0] == 0);
 
 wire [16:0] os_rom_addr = (os_download) ? {ioctl_addr[16:1],os_lsb} : abus_out[16:0];
 
@@ -526,7 +539,8 @@ wire [7:0] os_rom_din = (!os_lsb) ? ioctl_data[7:0] : ioctl_data[15:8];
 reg os_wren;
 wire [7:0] os_rom_dout;
 
-os_rom_bram	os_rom_bram_inst (
+spram #(.addr_width(17), .data_width(8), .mem_name("OS_R")) os_rom_bram_inst
+(
 	.clock   ( clk_sys ),
 
 	.address ( os_rom_addr ),
@@ -536,9 +550,7 @@ os_rom_bram	os_rom_bram_inst (
 	.q       ( os_rom_dout )
 );
 
-
 assign os_rom_q = (abus_out[16:0]==17'h0136E && status[2]) ? 8'h60 : os_rom_dout; // Patch the BEQ instruction to a BRA, to skip the cart checksum fail.
-
 
 reg os_lsb = 1;
 always @(posedge clk_sys) begin
@@ -554,17 +566,14 @@ always @(posedge clk_sys) begin
 	end
 end
 
-//assign VGA_DE = !vga_bl;
-//assign VGA_HS = !vga_hs_n;
-//assign VGA_VS = !vga_vs_n;
+reg vid_ce_div_2 = 0;
 
-assign VGA_HS = vga_hs_n ^ vga_vs_n;
-assign VGA_VS = vga_vs_n;
-
-assign VGA_R = vga_r;
-assign VGA_G = vga_g;
-assign VGA_B = vga_b;
-
+always @(posedge clk_sys) begin
+	if (vid_ce)
+		vid_ce_div_2 <= ~vid_ce_div_2;
+	if (reset)
+		vid_ce_div_2 <= 0;
+end
 
 assign CLK_VIDEO = clk_sys;
 wire CE_PIX = vid_ce;
@@ -572,15 +581,15 @@ wire CE_PIX = vid_ce;
 
 //assign VGA_SL = {~interlace,~interlace} & sl[1:0];
 
-video_mixer #(.LINE_LENGTH(640), .HALF_DEPTH(0)) video_mixer
+video_mixer #(.LINE_LENGTH(720), .HALF_DEPTH(0), .GAMMA(1)) video_mixer
 (
 	.CLK_VIDEO(CLK_VIDEO),      // input clk_sys
-	.ce_pix( CE_PIX ),          // input ce_pix
+	.ce_pix( vid_ce ),          // input ce_pix
 
 	.HDMI_FREEZE(0),
 	.freeze_sync(),
 
-	.scandoubler(1'b0),
+	.scandoubler(scale || forced_scandoubler),
 
 	.hq2x(scale==1),
 
@@ -596,11 +605,11 @@ video_mixer #(.LINE_LENGTH(640), .HALF_DEPTH(0)) video_mixer
 	.HBlank(hblank),            // input HBlank
 	.VBlank(vblank),            // input VBlank
 
-	// .VGA_R( VGA_R ),         // output [7:0] VGA_R
-	// .VGA_G( VGA_G ),         // output [7:0] VGA_G
-	// .VGA_B( VGA_B ),         // output [7:0] VGA_B
-	// .VGA_VS( VGA_VS ),       // output VGA_VS
-	// .VGA_HS( VGA_HS ),       // output VGA_HS
+	.VGA_R( VGA_R ),         // output [7:0] VGA_R
+	.VGA_G( VGA_G ),         // output [7:0] VGA_G
+	.VGA_B( VGA_B ),         // output [7:0] VGA_B
+	.VGA_VS( VGA_VS ),       // output VGA_VS
+	.VGA_HS( VGA_HS ),       // output VGA_HS
 	.VGA_DE( VGA_DE ),          // output VGA_DE
 	.CE_PIXEL(CE_PIXEL)
 );
@@ -614,7 +623,7 @@ assign AUDIO_L = aud_16_l;
 assign AUDIO_R = aud_16_r;
 
 // Cart reading is from DDR now...
-assign DDRAM_CLK = clk_sys;
+assign DDRAM_CLK = clk_ram;
 assign DDRAM_BURSTCNT = 1;
 
 // Jag DRAM is now mapped at 0x30000000 in DDR on MiSTer, hence the setting of the upper bits here.
@@ -741,7 +750,7 @@ sdram sdram
 (
 	.init(~pll_locked),
 
-	.clk( clk_sys ),				// Don't need the phase shift any more. DDIO is used to generate SDRAM_CLK instead (Sorg magic).
+	.clk( clk_ram ),				// Don't need the phase shift any more. DDIO is used to generate SDRAM_CLK instead (Sorg magic).
 
 	.SDRAM_DQ( SDRAM_DQ ),		// 16 bit bidirectional data bus
 	.SDRAM_A( SDRAM_A) ,			// 13 bit multiplexed address bus
